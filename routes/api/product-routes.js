8393ac5744bbe7770d44655738747dd578ffc196
Daniel Ruiz <danielruizgtz@gmail.com>
2021-11-15 15:46:17 -0600

    In this commit I'm updating the product-routes.js file to get all products or an specific one, to create a new product or to delete an existing one

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,30 +3,64 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// To get/find all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
-
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
-
-// create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+  Product.findAll({
+    attributes: ['id', 'product_name', 'stock', 'price'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  }).then(dbProduct => {
+    if (!dbProduct) {
+      res.status(404).json({ message: 'The product was not found :(' });
+      return;
     }
-  */
-  Product.create(req.body)
-    .then((product) => {
+    res.json(dbProduct);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
+});
+
+// To get/find one product
+router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'stock', 'price'],
+    include: [
+      {
+        model: Category,
+        attributes: ['cateogry_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  }).then(dbProduct => {
+    if (!dbProduct) {
+      res.status(404).json({ message: 'Product not found :(' });
+      return;
+    }
+    res.json(dbProduct);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
+});
+
+// To create a new product
+router.post('/', (req, res) => {
+  Product.create(req.body).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -47,7 +81,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// To update product information
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -89,8 +123,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
+//To delete a product
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbProduct => {
+    if (!dbProduct) {
+      res.status(404).json({ message: 'The product was not found :( '});
+      return;
+    }
+    res.json(dbProduct);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 module.exports = router;
