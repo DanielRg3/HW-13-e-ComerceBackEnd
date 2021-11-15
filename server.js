ae15a5d3f0320beb8a52fdb39ce077869c1d8bef
Daniel Ruiz <danielruizgtz@gmail.com>
2021-11-15 13:42:11 -0600

    In this commit I'm uploading the server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require("express");
-const routs = require("./routes")
+const routs = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!!!`);
+});
