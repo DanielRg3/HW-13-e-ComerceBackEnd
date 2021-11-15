5d61f6b5c1c4c9b362ffe1cca3117ee8ac17302b
Daniel Ruiz <danielruizgtz@gmail.com>
2021-11-15 16:15:05 -0600

    In this commit I'm updating the products.js file to define the parameters for each category

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,36 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    product_name: {
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    stock: {
+      type: DataTypes.DECIMAL,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: 'id',
+        model: 'category',
+      }
+    }
   },
   {
     sequelize,
